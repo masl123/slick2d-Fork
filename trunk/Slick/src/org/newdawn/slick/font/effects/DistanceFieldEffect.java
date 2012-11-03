65b843cca5ffa2bbf00d7169f7c474816db13f0b
Thomas ten Cate <ttencate@gmail.com>
2012-11-03 11:21:54 +0000

    Streamline and clean up DistanceFieldEffect and document it better

--- trunk/Slick/src/org/newdawn/slick/font/effects/DistanceFieldEffect.java
+++ trunk/Slick/src/org/newdawn/slick/font/effects/DistanceFieldEffect.java
@@ -30,9 +30,10 @@ import org.newdawn.slick.font.Glyph;
  */
 public class DistanceFieldEffect implements ConfigurableEffect
 {
+	// See getValues() for descriptions of these
 	private Color color = Color.white;
 	private int spread = 4;
-	private int downscale = 8;
+	private int upscale = 8;
 	
 	public Color getColor() {
 		return color;
@@ -50,12 +51,12 @@ public class DistanceFieldEffect implements ConfigurableEffect
 		this.spread = Math.max(1, spread);
 	}
 	
-	public int getDownscale() {
-		return downscale;
+	public int getUpscale() {
+		return upscale;
 	}
 	
-	public void setDownscale(int downscale) {
-		this.downscale = Math.max(1, downscale);
+	public void setUpscale(int upscale) {
+		this.upscale = Math.max(1, upscale);
 	}
 	
 	/**
@@ -65,7 +66,7 @@ public class DistanceFieldEffect implements ConfigurableEffect
 	 * @param y1 The y coordiante of the first point
  	 * @param x2 The x coordinate of the second point
 	 * @param y2 The y coordinate of the second point
-	 * @return The squared distance between two point
+	 * @return The squared distance between the two points
 	 */
 	private static int squareDist(final int x1, final int y1, final int x2, final int y2)
 	{
@@ -75,44 +76,46 @@ public class DistanceFieldEffect implements ConfigurableEffect
 	}
 	
 	/**
-	 * Process the image into a distance field
+	 * Process the image into a distance field.
 	 * 
-	 * @param inImage The image to process
-	 * @return The distance field image
+	 * The input image should be binary (black/white), but if not, any pixel with a value of over 128
+	 * in any of its color channels is considered opaque, and transparent otherwise.
+	 *  
+	 * The returned image is a factor of {@code upscale} smaller than {@code inImage}.
+	 * Opaque pixels more than {@link #spread} away in the output image from white remain opaque;
+	 * transparent pixels more than {@link #spread} away in the output image from black remain transparent.
+	 * In between, we get a smooth transition from opaque to transparent, with an alpha value of 0.5
+	 * when we are exactly on the edge.
+	 * 
+	 * @param inImage the image to process. 
+	 * @return the distance field image
 	 */
-	public BufferedImage process(BufferedImage inImage)
+	public BufferedImage computeDistanceField(BufferedImage inImage)
 	{
-		int outWidth = inImage.getWidth() / downscale;
-		int outHeight = inImage.getHeight() / downscale;
+		final int inWidth = inImage.getWidth();
+		final int inHeight = inImage.getHeight();
+		final int outWidth = inWidth / upscale;
+		final int outHeight = inHeight / upscale;
+		final BufferedImage outImage = new BufferedImage(outWidth, outHeight, BufferedImage.TYPE_4BYTE_ABGR);
 		
-		BufferedImage outImage = new BufferedImage(outWidth, outHeight, BufferedImage.TYPE_4BYTE_ABGR);
-		float[][] distances = new float[outWidth][outHeight];
-		
-		boolean[][] bitmap = new boolean[inImage.getWidth()][inImage.getHeight()];
-		for (int x = 0; x < inImage.getWidth(); ++x) {
-			for (int y = 0; y < inImage.getHeight(); ++y) {
+		// Note: coordinates reversed to mimic storage of BufferedImage, for memory locality
+		final boolean[][] bitmap = new boolean[inHeight][inWidth];
+		for (int y = 0; y < inHeight; ++y) {
+			for (int x = 0; x < inWidth; ++x) {
 				// Any colour with one of its channels greater than 128 is considered "inside"
-				bitmap[x][y] = (inImage.getRGB(x, y) & 0x808080) != 0;
+				bitmap[y][x] = (inImage.getRGB(x, y) & 0x808080) != 0;
 			}
 		}
 		
-		for (int x=0; x<outImage.getWidth(); x++)
+		for (int y = 0; y < outHeight; ++y)
 		{
-			for (int y=0; y<outImage.getHeight(); y++)
+			for (int x = 0; x < outWidth; ++x)
 			{
-				distances[x][y] = findSignedDistance( (x * downscale) + (downscale / 2),
-													  (y * downscale) + (downscale / 2),
-													  bitmap);
-			}
-		}
-
-		for (int x=0; x<outWidth; x++)
-		{
-			for (int y=0; y<outHeight; y++)
-			{
-				float d = distances[x][y];
-				int alpha = (int) (d * 255);
-				outImage.setRGB(x, y, (alpha << 24) | (color.getRGB() & 0xFFFFFF));
+				float signedDistance = findSignedDistance(
+						(x * upscale) + (upscale / 2),
+						(y * upscale) + (upscale / 2),
+						bitmap);
+				outImage.setRGB(x, y, distanceToRGB(signedDistance));
 			}
 		}
 		
@@ -120,78 +123,99 @@ public class DistanceFieldEffect implements ConfigurableEffect
 	}
 	
 	/**
-	 * Find the signed distance for a given point
+	 * For a distance as returned by {@link #findSignedDistance}, returns the corresponding "RGB" (really ARGB) color value.
+	 *  
+	 * @param signedDistance the signed distance of a pixel
+	 * @return an ARGB color value suitable for {@link BufferedImage#setRGB}.
+	 */
+	private int distanceToRGB(float signedDistance) {
+		float alpha = 0.5f + 0.5f * (signedDistance / spread);
+		alpha = Math.min(1, Math.max(0, alpha)); // compensate for rounding errors
+		int alphaByte = (int) (alpha * 0xFF); // no unsigned byte in Java :(
+		return (alphaByte << 24) | (color.getRGB() & 0xFFFFFF);
+	}
+	
+	/**
+	 * Returns the signed distance for a given point.
 	 * 
 	 * @param pointX The x coordinate of the point 
 	 * @param pointY The y coordinate of the point
-	 * @param bitmap The image on which the point exists
-	 * @return The signed distance
+	 * @param bitmap The upscaled binary glyph image
+	 * @return The signed distance, in units of pixels in the <em>output</em> image 
 	 */
 	private float findSignedDistance(final int pointX, final int pointY, boolean[][] bitmap)
 	{
-		final int width = bitmap.length;
-		final int height = bitmap[0].length;
-		final boolean base = bitmap[pointX][pointY];
+		final int width = bitmap[0].length;
+		final int height = bitmap.length;
+		final boolean base = bitmap[pointY][pointX];
 		
-		int maxDistance = downscale * spread;
-		final int startX = Math.max(0, pointX - maxDistance);
-		final int endX  = Math.min(width - 1, pointX + maxDistance);
-		final int startY = Math.max(0, pointY - maxDistance);
-		final int endY = Math.min(height - 1, pointY + maxDistance);
+		int maxDist = upscale * spread;
+		final int startX = Math.max(0, pointX - maxDist);
+		final int endX  = Math.min(width - 1, pointX + maxDist);
+		final int startY = Math.max(0, pointY - maxDist);
+		final int endY = Math.min(height - 1, pointY + maxDist);
 
-		final int squareRadius = maxDistance * maxDistance;
-		int closestSquareDist = squareRadius;
+		int closestSquareDist = maxDist * maxDist;
 		
-		for (int x=startX; x<=endX; x++)
+		for (int y = startY; y <= endY; ++y)
 		{
-			for (int y=startY; y<=endY; y++)
+			for (int x = startX; x <= endX; ++x)
 			{
-				final int sqDist = squareDist(pointX, pointY, x, y);
-				if (sqDist > squareRadius) {
-					continue;
-				}
-				if (base != bitmap[x][y] && sqDist < closestSquareDist)
+				if (base != bitmap[y][x])
 				{
-					closestSquareDist = sqDist;
+					final int squareDist = squareDist(pointX, pointY, x, y);
+					if (squareDist < closestSquareDist)
+					{
+						closestSquareDist = squareDist;
+					}
 				}
 			}
 		}
 		
 		float closestDist = (float) Math.sqrt(closestSquareDist);
-		if (base)
-		{
-			return 0.5f + 0.5f * Math.min(1.0f, closestDist / maxDistance);
-		}
-		else
-		{
-			return 0.5f - 0.5f * Math.min(1.0f, closestDist / maxDistance);
-		}
+		return (base ? 1 : -1) * closestDist / upscale;
+	}
+
+	/**
+	 * Draws the glyph to the given image, upscaled by a factor of {@link #upscale}.
+	 * 
+	 * @param image the image to draw to
+	 * @param glyph the glyph to draw
+	 */
+	private void drawGlyph(BufferedImage image, Glyph glyph) {
+		Graphics2D inputG = (Graphics2D) image.getGraphics();
+		inputG.setTransform(AffineTransform.getScaleInstance(upscale, upscale));
+		// We don't really want anti-aliasing (we'll discard it anyway),
+		// but accurate positioning might improve the result slightly
+		inputG.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+		inputG.setColor(Color.WHITE);
+		inputG.fill(glyph.getShape());
 	}
 
 	@Override
 	public void draw(BufferedImage image, Graphics2D g, UnicodeFont unicodeFont, Glyph glyph) {
-		int inputWidth = downscale * glyph.getWidth();
-		int inputHeight = downscale * glyph.getHeight();
+		BufferedImage input = new BufferedImage(
+				upscale * glyph.getWidth(),
+				upscale * glyph.getHeight(),
+				BufferedImage.TYPE_BYTE_BINARY);
+		drawGlyph(input, glyph);
 		
-		BufferedImage input = new BufferedImage(inputWidth, inputHeight, BufferedImage.TYPE_INT_ARGB);
-		Graphics2D inputG = (Graphics2D) input.getGraphics();
-		inputG.setTransform(AffineTransform.getScaleInstance(downscale, downscale));
-		// We don't really want anti-aliasing, but accurate positioning is nice
-		inputG.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-		inputG.setColor(Color.WHITE);
-		inputG.fill(glyph.getShape());
-		
-		BufferedImage distanceField = process(input);
+		BufferedImage distanceField = computeDistanceField(input);
 		
 		g.drawImage(distanceField, new AffineTransform(), null);
+	}
+	
+	@Override
+	public String toString() {
+		return "Distance field";
 	}
 
 	@Override
 	public List getValues() {
 		List values = new ArrayList();
 		values.add(EffectUtil.colorValue("Color", getColor()));
-		values.add(EffectUtil.intValue("Downscale", getDownscale(), "Downscale (even number recommended)"));
-		values.add(EffectUtil.intValue("Spread", getSpread(), "Spread (in pixels)"));
+		values.add(EffectUtil.intValue("Upscale", getUpscale(), "The distance field is computed from an image upscaled by this factor. Set this to a higher value for more accuracy, but slower font generation."));
+		values.add(EffectUtil.intValue("Spread", getSpread(), "The maximum distance from edges where the effect of the distance field is seen. Set this to about half the width of lines in your font."));
 		return values;
 	}
 
@@ -201,17 +225,12 @@ public class DistanceFieldEffect implements ConfigurableEffect
 			Value value = (Value)iter.next();
 			if (value.getName().equals("Color")) {
 				setColor((Color)value.getObject());
-			} else if (value.getName().equals("Downscale")) {
-				setDownscale((Integer)value.getObject());
+			} else if (value.getName().equals("Upscale")) {
+				setUpscale((Integer)value.getObject());
 			} else if (value.getName().equals("Spread")) {
 				setSpread((Integer)value.getObject());
 			}
 		}
 		
-	}
-	
-	@Override
-	public String toString() {
-		return "Distance field";
 	}
 }
